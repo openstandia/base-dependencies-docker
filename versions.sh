6419c289bc5cccb61ec58111fadaca7bf8af4c4b
Hiroyuki Wada <h2-wada@nri.co.jp>
2020-03-24 01:57:25 +0900

    feat: add build for keycloak-adapter-app-build

---
+++ versions.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export KEYCLOAK=9.0.0
+
+export KEYCLOAK_GRPC=0.3.0
+export MIDPOINT_GRPC=0.8.2
+
+export SPRING_BOOT=2.2.4.RELEASE
+export LETTUCE=4.5.0.Final
+
+export AWSSDK=2.10.73
+
+export ULID=8.2.0
+
